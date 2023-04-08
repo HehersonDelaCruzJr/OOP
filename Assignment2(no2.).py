b662d23026e17887bce2248a2ffdfcf5bff02a29
Heherson Dela Cruz <delacruzheherson4477@gmail.com>
2023-04-08 23:02:33 +0800

    update code

--- Assignment2(no2.).py
+++ Assignment2(no2.).py
@@ -21,29 +21,3 @@ for i in range (len(input_str)):
         output_str += input_str[i]
         
 print(output_str)
-
-
-Message = input("Message:")
-key = input("keyword:")
-
-Message_num = [ord(c) - 65 for c in Message]
-key_num = [ord(c) - 65 for c in key]
-
-key_rep = []
-key_num = 0
-
-for i in range(len(Message)):
-  key_rep.append(key_num[key_index])
-  key_index = (key_index + 1) % len(key)
-
-  ciphertext_num = []
-  for i in range(len(Message)):
-    mn = Message_num[i]
-    kr = key_rep[i]
-    ciphertext_num.append((mn + kr) % 26)
-
-    ciphertext = ''
-    for c in ciphertext_num:
-      ciphertext += chr(c + 65)
-
-print("\n", "\033[4m"+"\033[1;36m"+ciphertext)
